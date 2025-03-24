b9b6c960c9a67103fb6c7debf3688b2f74090879
PhuongNghi403 <nghihnp234111e@st.uel.edu.vn>
2025-03-24 20:51:37 +0700

    Delete KODEE_NetCoffeeManagement - TestDi directory
    up lại

--- KODEE_NetCoffeeManagement - TestDi/test/test_employee_screen.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from ui.ui_ext_files.employee_screen_ext import employee_screen_ext
-
-app=QApplication([])
-myWindow= employee_screen_ext()
-myWindow.setupUi(QMainWindow())
-myWindow.showWindow()
-app.exec()
--- KODEE_NetCoffeeManagement - TestDi/test/test_login_screen.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from ui.ui_ext_files.login_screen_ext import login_screen_ext
-
-app=QApplication([])
-myWindow= login_screen_ext()
-myWindow.setupUi(QMainWindow())
-myWindow.showWindow()
-app.exec()
