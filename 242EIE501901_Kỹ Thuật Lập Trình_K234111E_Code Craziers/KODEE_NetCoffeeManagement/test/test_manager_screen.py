5762f033d8105f3e9b6537e8da9a7a62a20a813f
PhuongNghi403 <nghihnp234111e@st.uel.edu.vn>
2025-03-24 22:25:54 +0700

    Delete 242EIE501901_Kỹ Thuật Lập Trình_K234111E_Code Craziers/KODEE_NetCoffeeManagement directory
    delete

--- 242EIE501901_Kỹ Thuật Lập Trình_K234111E_Code Craziers/KODEE_NetCoffeeManagement/test/test_manager_screen.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from ui.ui_ext_files.manager_screen_ext import manager_screen_ext
-
-app=QApplication([])
-myWindow= manager_screen_ext()
-myWindow.setupUi(QMainWindow())
-myWindow.showWindow()
-app.exec()
